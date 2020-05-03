854293fbb06bf0092d834ef02dea10a1f65af91e
KeenanField <keenan.a.field@gmail.com>
2020-05-03 14:06:08 +1000

    second hello message

--- src/main/java/jar/App.java
+++ src/main/java/jar/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Test Message to Teamcity WOOT" );
+        System.out.println( "Second hello message");
     }
 }
